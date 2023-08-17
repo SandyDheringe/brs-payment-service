d34b2a416aac72a1ff1e2b93185f73597ca26ee1
sandeep <sandeep.ariseup@gmail.com>
2023-08-17 18:34:21 +0530

    Initial commit

---
+++ src/main/java/com/brspayment/BrsPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brspayment/BrsPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brspayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
